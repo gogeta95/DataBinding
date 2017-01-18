8a62bdaba40cffc3000fda7bddb48ca9a472ed41
Saurabh Singhal <singhalsaurabh95@gmail.com>
2017-01-18 19:34:19 +0530

    added custom binding class.

--- app/src/main/java/com/example/saurabh/databinding/MainActivity.java
+++ app/src/main/java/com/example/saurabh/databinding/MainActivity.java
@@ -4,14 +4,14 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.saurabh.databinding.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        MainActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         User user = new User("Saurabh", "Singhal", "go");
         binding.setUser(user);
         MyHandlers handlers = new MyHandlers();
