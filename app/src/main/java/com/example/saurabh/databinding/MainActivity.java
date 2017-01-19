1e45e7c6ef745841d4f95032f5a4cf511b06effa
Saurabh Singhal <singhalsaurabh95@gmail.com>
2017-01-19 15:30:00 +0530

    added map, list and string concat.

--- app/src/main/java/com/example/saurabh/databinding/MainActivity.java
+++ app/src/main/java/com/example/saurabh/databinding/MainActivity.java
@@ -4,6 +4,10 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MainActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        User user = new User("Saurabh", "Singhal", "go");
-        binding.setUser(user);
+        List<User> users = new ArrayList<>();
+        Map<Integer, User> usersmap = new HashMap<>();
+        for (int i = 0; i < 6; i++) {
+            User user = new User("User " + i, null, "List " + i);
+            users.add(user);
+            user = new User("User " + i, null, "Map " + i);
+            usersmap.put(i, user);
+        }
+        binding.setUsers(users);
+        binding.setUsersmap(usersmap);
         MyHandlers handlers = new MyHandlers();
         binding.setHandler(handlers);
     }
